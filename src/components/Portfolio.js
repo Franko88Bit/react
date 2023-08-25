6e11d34f34e4aa6983311aca054501c1cb6cd450
franko88bit <franks188@icloud.com>
2023-08-24 23:16:24 -0400

    uploaded src

---
+++ src/components/Portfolio.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+        </div>
+      </div>
+  </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,128 @@
+let resumeData = {
+    "name": "Frank Alvarez",
+    "role": "Full-stack software developer",
+    "roleDescription": "I like frontend and backend technologies and like to learn about new technologies.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/frank-alvarez-091b19271/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Franko88Bit",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I have a dog named June.",
+    "address":"Miami, FL",
+    "website":"https://github.com/Franko88Bit",
+    "education":[
+      {
+        "UniversityName":"University of Miami",
+        "specialization":"Coding Bootcamp",
+        "MonthOfPassing":"September",
+        "YearOfPassing":"2023"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":" Project 1",
+        "specialization":"https://github.com/Franko88Bit/FrankO-PWA-Text-Editor"
+      },
+      {
+        "CompanyName":" Project 2",
+        "specialization":"https://github.com/Franko88Bit/FrankO-MVC-Tech-Blog"
+      },
+      {
+        "CompanyName":" Project 3",
+        "specialization":"https://github.com/Franko88Bit/FrankO-E-Commerce-Site"
+      },
+      {
+        "CompanyName":"Project 4",
+        "specialization":"https://github.com/Franko88Bit/FrankO-Social-Network-API"
+      },
+      {
+        "CompanyName":"Project 5",
+        "specialization":"https://github.com/Franko88Bit/FrankO-REGEX-Tutorial"
+      },
+      {
+        "CompanyName":"Project 6",
+        "specialization":"https://github.com/Franko88Bit/FrankO_Beer_Company"
+      },
+      {
+        "CompanyName":"Project 7",
+        "specialization":"https://github.com/Franko88Bit/FrankO_Note_Taker"
+      },
+      {
+        "CompanyName":"Project 8",
+        "specialization":"https://github.com/Franko88Bit/FrankO_LOGO_Maker"
+      },
+      {
+        "CompanyName":"Project 9",
+        "specialization":"https://github.com/Franko88Bit/FRANKO-ReadMe-Generator"
+      },
+      {
+        "CompanyName":"Project 10",
+        "specialization":"https://github.com/Franko88Bit/Frank-Alvarez-Portfolio-Hiring"
+      },
+      {
+        "CompanyName":"Project 11",
+        "specialization":"https://github.com/Franko88Bit/FrankO-Weather-API-Dashboard"
+      },
+      {
+        "CompanyName":"Project 12",
+        "specialization":"https://github.com/Franko88Bit/FrankO-Calender"
+      },
+      {
+        "CompanyName":"Project 13",
+        "specialization":"https://github.com/Franko88Bit/Bumble-Bee-Coding-Quiz"
+      },
+      {
+        "CompanyName":"Project 14",
+        "specialization":"https://github.com/Franko88Bit/Titanium-Password-Generator"
+      },
+      {
+        "CompanyName":"Project 15",
+        "specialization":"https://github.com/Franko88Bit/Frank-Alvarez-Module-1"
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+    ]
+  }
+  
+  export default resumeData
